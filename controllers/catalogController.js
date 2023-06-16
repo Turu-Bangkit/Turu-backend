92daa958a1b40e780f7f1eaa7d5b62f6951648a1
kdelia12 <wild12748@gmail.com>
2023-06-16 16:25:08 +0700

    adding pulsa rewards payment api

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,4 +1,10 @@
+require('dotenv').config();
+const CryptoJS = require("crypto-js");
+const axios = require("axios");
 const admin = require("../firebaseAdmin");
+const apiKey = process.env.API_KEY;
+const username = process.env.usernameapi;
+
 
 const getCatalog = async (req, res) => {
   const catalog = admin.database().ref("catalogs");
@@ -34,13 +40,51 @@ const getDetailCatalog = async (req, res) => {
   }
 };
 
+// const exchangePoint = async (req, res) => {
+//   const { uid } = req.params;
+//   const { idCatalog } = req.body;
+//   const user = admin.database().ref(`users/${uid}`);
+//   const userPoint = (await user.child("point").once("value")).val();
+//   const point = (await admin.database().ref(`catalogs/${idCatalog}/point`).once("value")).val();
+//   const price = (await admin.database().ref(`catalogs/${idCatalog}/price`).once("value")).val();
+//   const product_code = `pulsa${price}`;
+  
+//   if (!(await user.once("value")).exists()) {
+//     res.json({ error: true, message: "User Not Found" });
+//     return;
+//   }
+//   if (!(await admin.database().ref(`catalogs/${idCatalog}`).once("value")).exists()) {
+//     res.json({ error: true, message: "Catalog Not Found" });
+//     return;
+//   }
+
+//   if (userPoint < point) {
+//     const response = {
+//       error: true,
+//       message: "Point is not enough",
+//     };
+//     res.json(response);
+//   } else {
+//     user.update({
+//       point: userPoint - point,
+//     });
+//     const response = {
+//       error: false,
+//       message: "Success Exchange Point",
+//     };
+//     res.json(response);
+//   }
+// };
+
 const exchangePoint = async (req, res) => {
   const { uid } = req.params;
   const { idCatalog } = req.body;
   const user = admin.database().ref(`users/${uid}`);
   const userPoint = (await user.child("point").once("value")).val();
   const point = (await admin.database().ref(`catalogs/${idCatalog}/point`).once("value")).val();
-  
+  const price = (await admin.database().ref(`catalogs/${idCatalog}/price`).once("value")).val();
+  const product_code = `pulsa${price}`;
+
   if (!(await user.once("value")).exists()) {
     res.json({ error: true, message: "User Not Found" });
     return;
@@ -57,16 +101,52 @@ const exchangePoint = async (req, res) => {
     };
     res.json(response);
   } else {
-    user.update({
-      point: userPoint - point,
-    });
-    const response = {
-      error: false,
-      message: "Success Exchange Point",
+    // Generate the ref_id value
+    const now = new Date();
+    const ref_id = now.toISOString().replace(/[^0-9]/g, "");
+
+    // Generate the sign value with additional randomness
+    const sign = CryptoJS.MD5(username+apiKey+ref_id).toString();
+    // Define the request payload
+    const payload = {
+      customer_id: '081354544844',
+      product_code: product_code,
+      ref_id: ref_id,
+      username: username,
+      sign: sign
     };
-    res.json(response);
-  }
+
+    // Make the API call
+    axios.post("https://prepaid.iak.id/api/top-up", payload)
+    .then(apiResponse => {
+      console.log(product_code)
+      console.log(apiKey)
+      console.log(username)
+      console.log(apiResponse.data);
+
+      user.update({
+        point: userPoint - point,
+      });
+      const successResponse = {
+        error: false,
+        message: "Success Exchange Point",
+      };
+      res.json(successResponse);
+    })
+    .catch(error => {
+      console.error(error);
+      // Handle the error here
+      const errorResponse = {
+        error: true,
+        message: "Failed to Exchange Point",
+      };
+      res.json(errorResponse);
+    });
+}
 };
+
+
+module.exports = exchangePoint;
 
 
 module.exports = { getCatalog, getDetailCatalog, exchangePoint };
